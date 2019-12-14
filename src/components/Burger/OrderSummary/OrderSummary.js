451d7981dcf79706e1186ecf9f982675a7404f5c
Josh Garcia <josh2397garcia@gmail.com>
2019-12-14 17:48:49 -0500

    Add console log for componentDidUpdate()

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,10 @@ import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
     render () {
         const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
